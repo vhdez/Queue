f2c1b5efe9e555f0b4494f3d0dbd921eff3bb5d8
cancri24 <cirwindiehl@gmail.com>
2017-12-07 15:04:12 -0500

    initial commit

--- src/edu/sla/Main.java
+++ src/edu/sla/Main.java
@@ -1,15 +1,18 @@
 package edu.sla;
 
-import static edu.sla.Main.myQueue;
-
 public class Main {
     static String[] myQueue = new String[100];
 
     public static void main(String[] args) {
+        QueuePopulator qpop = new QueuePopulator();
+        Thread populate = new Thread(qpop);
 
+        QueuePrinter qprint = new QueuePrinter(populate);
+        Thread print = new Thread(qprint);
+
+        print.start();
+        populate.start();
 
     }
 
 }
-
-
