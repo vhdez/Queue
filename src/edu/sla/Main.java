94d1f30775360adce37297dc8ea6aa9bb798e0f4
cancri24 <cirwindiehl@gmail.com>
2017-12-11 09:01:33 -0500

    initial commit

--- src/edu/sla/Main.java
+++ src/edu/sla/Main.java
@@ -2,11 +2,11 @@ package edu.sla;
 
 public class Main {
 
-
     public static void main(String[] args) {
+        Queue theQueue = new Queue();
 
-        QueuePopulator qpop = new QueuePopulator();
-        QueuePrinter qprint = new QueuePrinter();
+        QueuePopulator qpop = new QueuePopulator(theQueue);
+        QueuePrinter qprint = new QueuePrinter(theQueue);
         Thread populate = new Thread(qpop);
         Thread print = new Thread(qprint);
 
--- src/edu/sla/QueuePopulator.java
+++ src/edu/sla/QueuePopulator.java
@@ -1,8 +1,16 @@
 package edu.sla;
 
 public class QueuePopulator implements Runnable {
-    private int timesRun = 0;
+    private  Queue theQueue;
+
+    QueuePopulator(Queue queue) {
+        theQueue = queue;
+    }
+
     public void run() {
-        timesRun++;
+        for(int i = 0; i < 3; i++) {
+            while (!theQueue.getQueue()[theQueue.getQueue().length - 1].equals("0")) ;
+            theQueue.populate();
+        }
     }
 }
